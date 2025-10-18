7dbd724382e8e12d6f137daa704197169d96329a
KimChanaDev <chanaweestr@gmail.com>
2025-10-18 15:04:16 +0700

    feat: hero section

---
+++ src/lib/components/ChapterPreview.svelte
@@ -0,0 +1,1 @@
+<h1>Chapter Review</h1>
---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import bookCover from '$assets/book_cover.png';
+    import phoneCovel from '$assets/phone_cover.png';
+    import { Button } from '$components';
+</script>
+
+<section class="hero">
+    <div class="hero-showcase text-center bold">
+        <h1 class="purple">MOVE TO</h1>
+        <h1 class="purple mb-xs">PAIN</h1>
+        <h4 class="italic semi-bold">There's no need to make the same mistake I take.</h4>
+        <img class="book-cover" src={bookCover} alt="">
+        <img class="phone-cover" src={phoneCovel} alt="">
+    </div>
+    <div class="hero-text white text-center">
+        <h1>THIS EBOOK WILL</h1>
+        <h1>SAVE YOU MONEY,</h1>
+        <h1 class="mb-m">TIME & NERVES</h1>
+        <p class="light-grey mb-l">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Veritatis consequatur temporibus nobis minima quasi magni, 
+            at, modi deserunt sunt eius perferendis eaque optio 
+            doloribus harum adipisci mollitia, a illum facere.
+        </p>
+        <Button>Purchase E-book for 300 Bath</Button>
+    </div>
+</section>
+
+<style>
+    .hero {
+        display: flex;
+        height: 100vh;
+        overflow-y: hidden;
+    }
+
+    .hero-showcase {
+        position: relative;
+        width: 50%;
+        padding: 100px 16px 0 16px;
+        background: linear-gradient(
+            135deg,
+            #e2cbff,
+            #e2cbff 25%,
+            #deeaff 50%,
+            #eee5ff 75%,
+            #927bcc
+        );
+    }
+
+    .hero-text {
+        padding: 100px 5%;
+        width: 50%;
+        background-color: black;
+    }
+
+    .book-cover {
+        position: absolute;
+        right: 2%;
+        bottom: 0;
+        width: 65%;
+        transform: translateY(30%);
+    }
+
+    .phone-cover {
+        position: absolute;
+        width: 35%;
+        bottom: 0;
+        right: 52%;
+        transform: translateY(25%);
+    }
+</style>
