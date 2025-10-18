285b12c0afcdfbc04a842202ad985f7af0e11374
KimChanaDev <chanaweestr@gmail.com>
2025-10-19 00:13:49 +0700

    feat: integrate Stripe for checkout process and add dependencies

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,25 +1,42 @@
 <script lang="ts">
-    let { children, ...props } = $props();
+	import { goto } from '$app/navigation';
+	let { children, ...props } = $props();
 
+	async function onclick() {
+		try {
+			const response = await fetch('/api/checkout', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			const { url } = await response.json();
+			if (url) {
+				window.location.href = url;
+			}
+		} catch (error) {
+			goto('/checkout/failure');
+		}
+	}
 </script>
 
-<button {...props}>{@render children()}</button>
+<button {...props} {onclick}>{@render children()}</button>
 
 <style>
-    button {
-        background-color: black;
-        color: white;
-        padding: 20px 24px;
-        font-weight: normal;
-        font-size: 22px;
-        text-transform: uppercase;
-        transition: all 0.3s;
-        border: 1px solid white;
-    }
+	button {
+		background-color: black;
+		color: white;
+		padding: 20px 24px;
+		font-weight: normal;
+		font-size: 22px;
+		text-transform: uppercase;
+		transition: all 0.3s;
+		border: 1px solid white;
+	}
 
-    button:hover {
-        background-color: white;
-        color: black;
-        cursor: pointer;
-    }
+	button:hover {
+		background-color: white;
+		color: black;
+		cursor: pointer;
+	}
 </style>
